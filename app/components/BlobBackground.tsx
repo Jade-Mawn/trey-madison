8672f50be727cf2b1d0e020dbfe9271b872e9ab1
JadeMawn <jade.mawn@richmond.edu>
2026-02-08 22:13:30 -0500

    stupid freakin blobs

--- app/components/BlobBackground.tsx
+++ app/components/BlobBackground.tsx
@@ -4,7 +4,7 @@ const BlobBackground: React.FC = () => {
   return (
     <div className="absolute inset-0 overflow-hidden -z-10">
       <img
-        src="/Blobs.svg"
+        src="/Blobs.jpeg"
         alt="Blob Background"
         className="w-full h-full object-cover filter blur-3xl"
       />
