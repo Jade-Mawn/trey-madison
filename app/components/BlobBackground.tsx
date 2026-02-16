b43a139d56afadf8414f9d095ba1a8b33887214b
JadeMawn <jade.mawn@richmond.edu>
2026-02-15 22:51:58 -0500

    Background fix?

--- app/components/BlobBackground.tsx
+++ app/components/BlobBackground.tsx
@@ -4,7 +4,7 @@ const BlobBackground: React.FC = () => {
   return (
     <div className="absolute inset-0 overflow-hidden -z-10">
       <img
-        src="/university of richmond .jpg"
+        src="/university-of-richmond.jpg"
         alt="Blob Background"
         className="w-full h-full object-cover"
       />
